c9dfaf885fba962fa039ba282fe746b937ea4275
vibha <vibhauc@gmail.com>
2022-02-22 15:59:33 +0530

    Adding the welcome message

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AddressBook
+{
+    class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNunmber { get; set; }
+        public string eMail { get; set; }
+    }
+}
